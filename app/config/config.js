b323122109cd2c7ac3799a57aa7b28eeab8b963a
Kris Madsen <kris.madsen@kmadsen-osx.cbmain.clarabridge.com>
2018-08-08 16:25:30 -0400

    masters only

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   'base': 'http://',
-  'endpoints': ['d5-e01-lmes04', 'd5-e01-les01', 'd5-e02-lmes01', 'd5-e02-lmes02', 'd5-e02-lmes03', 'd5-e01-lmes05', 'd5-e01-lmes06', 'd5-e02-lmes03', 'd5-e04-lmes01', 'd5-e04-lmes02'],
-  'pollTime': 60000 * 5,
+  'endpoints': ['d5-e01-lmes04', 'd5-e02-lmes01', 'a1-e01-les10'],
+  'pollTime': 60000 * 1,
   'query': ':9200/_cat/health?h=status'
 }
 
